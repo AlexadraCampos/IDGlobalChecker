a9213c7ca3d47086c8376014725fa9506be318fc
Alexandra Cordeiro Campos <srcoelho007@gmail.com>
2025-02-01 18:20:50 -0300

    Create idgc.py
    Primeira Commit do Projeto

---
+++ idgc.py
@@ -0,0 +1,33 @@
+class IDGC:
+    def Brazil_CPF_Validator(self, cpf: str) -> bool:
+        # Remove caracteres não numéricos
+        cpf = ''.join(filter(str.isdigit, cpf))
+
+        # Verifica se o CPF tem 11 dígitos ou se todos os dígitos são iguais
+        if len(cpf) != 11 or cpf == cpf[0] * 11:
+            return False
+
+        # Calcula o primeiro dígito verificador
+        soma = sum(int(cpf[i]) * (10 - i) for i in range(9))
+        digito1 = 11 - (soma % 11)
+        if digito1 >= 10:
+            digito1 = 0
+
+        # Calcula o segundo dígito verificador
+        soma = sum(int(cpf[i]) * (11 - i) for i in range(10))
+        digito2 = 11 - (soma % 11)
+        if digito2 >= 10:
+            digito2 = 0
+
+        # Verifica se os dígitos calculados conferem com os dígitos finais do CPF
+        return cpf[-2:] == f"{digito1}{digito2}"
+
+
+jif = IDGC()
+cpf = "015.693.756-98"
+retorno = jif.Brazil_CPF_Validator(cpf)
+
+if retorno:
+    print("CPF válido")
+else:
+    print("CPF inválido")
