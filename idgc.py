5877a69b168cd03cd45216a07ad8844ed892d42d
Lucas Romualdo Fernandes <31623291+lucasromualdo@users.noreply.github.com>
2025-02-13 22:27:37 -0300

    Inclusão do CUIL Argentino Validador

--- idgc.py
+++ idgc.py
@@ -42,21 +42,34 @@ class IDGC:
         return cnpj[-2:] == f"{digito1}{digito2}"
 
     def Argentina_DNI_Validator(self, dni: str) -> bool:
-
-        dni = re.sub(r'\D', '', dni)  # Remove caracteres não numéricos
-
-        # Verifica se o DNI tem 7 ou 8 dígitos
+        dni = re.sub(r'\D', '', dni)
         if not dni.isdigit() or len(dni) not in [7, 8]:
             print("DNI deve conter 7 ou 8 dígitos numéricos!")
             return False
-        
         return True
     
+    def Argentina_CUIL_Validator(self, cuil: str) -> bool:
+        cuil = re.sub(r'\D', '', cuil)
+        if not cuil.isdigit() or len(cuil) != 11:
+            print("CUIL deve conter 11 dígitos numéricos!")
+            return False
+        
+        coeficientes = [2, 3, 4, 5, 6, 7, 2, 3, 4, 5]
+        soma = sum(int(cuil[i]) * coeficientes[i] for i in range(10))
+        digito_verificador = 11 - (soma % 11)
+        if digito_verificador == 11:
+            digito_verificador = 0
+        elif digito_verificador == 10:
+            digito_verificador = 9
+        
+        return int(cuil[-1]) == digito_verificador
 
 # Testando a validação
 jif = IDGC()
 cpf = "306.559.490-03"
 cnpj = "11.222.333/0001-81"
+dni = "12345678"
+cuil = "20-12345678-3"
 
 if jif.Brazil_CPF_Validator(cpf):
     print("CPF válido")
@@ -67,3 +80,13 @@ if jif.Brazil_CNPJ_Validator(cnpj):
     print("CNPJ válido")
 else:
     print("CNPJ inválido")
+
+if jif.Argentina_DNI_Validator(dni):
+    print("DNI válido")
+else:
+    print("DNI inválido")
+
+if jif.Argentina_CUIL_Validator(cuil):
+    print("CUIL válido")
+else:
+    print("CUIL inválido")
